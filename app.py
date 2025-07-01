ccc533cc210398aa6ff9bbcf261e37f571ffa6cb
Blindmanzeye <shafatiscool@gmail.com>
2025-07-01 11:07:39 -0400

    Implemented data fetching for the app. Now we just need to proccess and display

--- app.py
+++ app.py
@@ -2,9 +2,11 @@ import tkinter
 import requests
 import datetime
 from geopy.geocoders import Nominatim
-from timezonefinder import TimezoneFinder 
+from timezonefinder import TimezoneFinder
+import re
+import json
 
-print(datetime.datetime.now())
+requestedDate: str = str(datetime.datetime.now())
 # 2025-06-30 19:22:45.018807
 geolocator = Nominatim(user_agent="geoapiExcercises")
 with open("region.txt", "r") as f:
@@ -13,11 +15,30 @@ with open("region.txt", "r") as f:
 if location:
     lat = location.latitude
     lon = location.longitude
-    print(f"Latitude: {lat}, Longitude: {lon}")
+    # Latitude: 43.6534817, Longitude: -79.3839347
 else:
     print("City not found")
     raise LookupError
 
 tf = TimezoneFinder()
 timezone = tf.timezone_at(lng=lon, lat=lat)
-print(f"Timezone: {timezone}")
+# Timezone: America/Toronto
+
+date: list = re.split("- | ", requestedDate)
+strDate = str(date.pop(0))
+strDateReversed = "-".join(reversed(strDate.split("-")),)
+# 01-07-2025
+
+
+link = "https://api.aladhan.com/v1/timings"
+date = strDateReversed
+
+
+officialLink = f"{link}/{date}?latitude={lat}&longitude={lon}&method=3&shafaq=general&tune=0%2C0%2C5%2C7%2C9%2C-1%2C0%2C8%2C-6&school=1&timezonestring={timezone}&calendarMethod=UAQ"
+print(officialLink)
+response = requests.get(officialLink)
+if response.status_code == 200:
+    data = response.text
+else:
+    print(f"Error: {response}")
+    raise LookupError
