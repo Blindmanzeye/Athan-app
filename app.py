5fcf029d48d0db5460e262a0449968937ca8f014
Blindmanzeye <shafatiscool@gmail.com>
2025-07-02 04:21:02 -0400

    Finished backend with api calls, local data checks to limit api calls, data parsing and relevant variable assignment TODO: Build Front End

--- app.py
+++ app.py
@@ -7,9 +7,21 @@ import re
 import json
 
 
+def loadJson() -> dict:
+    with open("data.json", "r") as jsonData:
+        data = json.load(jsonData)
+        return data
+
+
+def writeToJson(data: dict) -> None:
+    with open("data.json", "w") as jsonObj:
+        json.dump(data, jsonObj, indent=4)
+
+
 def findTimezone(lat: float, lon: float) -> str:
     tf = TimezoneFinder()
     timezone = tf.timezone_at(lng=lon, lat=lat)
+    return timezone
 
 
 def findDate() -> str:
@@ -34,6 +46,29 @@ def findCoordinates() -> tuple:
     return (lat, lon)
 
 
+def parseData(data: dict) -> dict:
+    times: dict = data["data"]["timings"]
+    dateData: dict = data["data"]["date"]
+
+    islamicDateData: dict = dateData["hijri"]
+    islamicMonthData: dict = islamicDateData["month"]
+    islamicDate: str = islamicDateData["date"]
+    islamicData  = {
+        "month" : islamicMonthData,
+        "date": islamicDate
+    }
+
+    weekday: str = dateData["gregorian"]["weekday"]["en"]
+    date: str = dateData["gregorian"]["date"]
+    
+    del times["Imsak"]
+    del times["Firstthird"]
+    del times["Lastthird"]
+
+    returnData = {"islamicData": islamicData, "weekday": weekday, "times": times, "date": date}
+    return returnData
+
+
 def main() -> None:
     link = "https://api.aladhan.com/v1/timings"
     date = findDate()
@@ -42,14 +77,27 @@ def main() -> None:
     lon = coordinates[1]
     timezone = findTimezone(lat, lon)
 
-    officialLink = f"{link}/{date}?latitude={lat}&longitude={lon}&method=3&shafaq=general&tune=0%2C0%2C5%2C7%2C9%2C-1%2C0%2C8%2C-6&school=1&timezonestring={timezone}&calendarMethod=UAQ"
-    print(officialLink)
-    response = requests.get(officialLink)
-    if response.status_code == 200:
-        data = response.text
+    officialLink = f"{link}/{date}?latitude={lat}&longitude={lon}&method=3&shafaq=general&tune=0%2C32%2C-2%2C0%2C0%2C0%2C0%2C-20%2C0&school=1&timezonestring={timezone}&calendarMethod=UAQ"
+    tempData = loadJson()
+    if date == tempData["date"]:
+        print("Data Pulled from Local File")
+        parsedData = tempData
     else:
-        print(f"Error: {response}")
-        raise LookupError
+        response = requests.get(officialLink)
+        if response.status_code == 200:
+            print("Data Pulled from API")
+            data = json.loads(response.text)
+            parsedData: dict = parseData(data)
+        else:
+            print(f"Error: {response}")
+            raise LookupError
+    
+    writeToJson(parsedData)
+    islamicDate: str = parsedData["islamicData"]["date"]
+    islamicMonth: str = parsedData["islamicData"]["month"]["en"]
+    weekday: str = parsedData["weekday"]
+    prayerTimes: dict = parsedData["times"]
+
 
 if __name__ == "__main__":
     main()
--- readme.md
+++ readme.md
@@ -4,3 +4,22 @@ Takes live times from the data provided by aladhan.com
 Displays it using tkinter
 
 enter your city into the region.txt file. JUST YOUR CITY
+
+Please Make sure you adjust the "tune" parameter in the link, the format is as follows numbers are in minutes
+also note that ismak got ommited from the data during the parsing section
+also, each part is separated by %2C
+Imsak,Fajr,Sunrise,Dhuhr,Asr,Maghrib,Sunset,Isha,Midnight
+so if you want to adjust ismak by +1 min, fajr by 0 min, sunrise by 0 min, dhuhr by +20 min, asr by +20 min, maghrib by +5 min, sunset by 0 min, isha by -5 min, and midnight by -10min, your link would be
+
+&tune=1%2C0%2C0%2C20%2C20%2C5%2C0%2C-5%2C-10&
+or, use this formatted string
+ismakadj = 1
+fajradj = 0
+sunriseadj = 0
+dhuhradj = 20
+asradj = 20
+maghribadj = 5
+sunsetadj = 0
+ishaadj = -5
+midnightadj = -10
+link = rf"&tune={ismakadj}%2C{fajradj}%2C{sunriseadj}%2C{dhuhradj}%2C{asradj}%2C{maghribadj}%2C{sunsetadj}%2C{ishaadj}%2C{midnightadj}&"
